34c99a2e8b217f7be016e35e44468f9fb35d0005
bac <bacdo0122@gmail.com>
2022-01-02 19:33:58 +0700

    as

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ const like = require("./router/like");
 const comment = require("./router/comment");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-
+var whitelist = [
+  "http://localhost:3000",
+  "https://amazon-frontend1.vercel.app/",
+];
 const firbaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://function-28be9-default-rtdb.firebaseio.com",
 });
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionsSuccessStatus: 200,
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 app.use(cors(corsOptions));
